743ce3c646ba0153ded1fb584464010c6ed8c436
smilejsu82 <smilejsu82@gmail.com>
2026-04-28 16:53:21 +0900

    [Add] PlayerBullet 위쪽 이동 로직 추가

---
+++ spaceshooter/Assets/smilejsu/Scripts/PlayerBullet.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed * Time.deltaTime);
+    }
+}
