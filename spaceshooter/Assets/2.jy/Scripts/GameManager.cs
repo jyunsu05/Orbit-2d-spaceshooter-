c172d1589722f7a0ede2e68d9fcb6bcb3c849683
softCastella <agelin50@gmail.com>
2026-04-28 18:32:42 +0900

    jy: 폴더 넘버링

---
+++ spaceshooter/Assets/2.jy/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //UI관련
+    public GameObject life_0;
+    public GameObject life_1;
+    public GameObject life_2;
+    public GameObject boomLife_0;
+    public GameObject boomLife_1;
+    public GameObject boomLife_2;
+    
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
